0e3404d23677c9a8923caf571ccbac743f28731c
Bartha Levente <b.levi.levi@gmail.com>
2025-04-21 19:48:05 +0200

    Chatgpt nélküli design kész egy izomcsoprt gyakorlatához, de a hozzá tartozó js megírása még nem kezdődött el.

---
+++ src/mell.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-app.js";
+import { getDatabase, ref, set, get } from "https://www.gstatic.com/firebasejs/9.8.0/firebase-database.js";
+
+// Firebase konfiguráció
+const firebaseConfig = {
+  apiKey: "AIzaSyD4okJhAEH6HjxsH84mynAtMhXggxK2URI",
+  authDomain: "gymapp-93351.firebaseapp.com",
+  databaseURL: "https://gymapp-93351-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "gymapp-93351",
+  storageBucket: "gymapp-93351.firebasestorage.app",
+  messagingSenderId: "679760443477",
+  appId: "1:679760443477:web:76d9b1217b67f6df2fbcdc"
+};
+
+// Firebase alkalmazás inicializálása
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
